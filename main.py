1e4aaba59223ad2c79e3651c3107f0bf9540727b
Seano <sean01234@yahoo.com>
2023-08-25 23:37:31 -0400

    get investment value

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sqlite3
+import datetime
+
 import requests
 import click
 
@@ -26,6 +29,44 @@ def cli():
 
 
 @click.command()
+@click.option("--coin_id")
+@click.option("--currency")
+@click.option("--amount", type=float)
+@click.option("--sell", is_flag=True)
+def add_investment(coin_id, currency, amount, sell):
+    sql = "INSERT INTO investments VALUES (?, ?, ?, ?, ?);"
+    values = (coin_id, currency, amount, sell, datetime.datetime.now())
+    cursor.execute(sql, values)
+    database.commit()
+
+    if sell:
+        print(f"Added sell of {amount} {coin_id}")
+    else:
+        print(f"Added buy of {amount} {coin_id}")
+
+
+@click.command()
+@click.option("--coin_id")
+@click.option("--currency")
+def get_investment_value(coin_id, currency):
+    coin_price = get_coin_price(coin_id, currency)
+    sql = """SELECT amount
+    FROM investments
+    WHERE coin_id=?
+    AND currency=?
+    AND sell=?;"""
+    buy_result = cursor.execute(sql, (coin_id, currency, False)).fetchall()
+    sell_result = cursor.execute(sql, (coin_id, currency, True)).fetchall()
+    buy_amount = sum([row[0] for row in buy_result])
+    sell_amount = sum([row[0] for row in sell_result])
+
+    total = buy_amount - sell_amount
+
+    print(
+        f"You own a total of {total} {coin_id} worth {total * coin_price} {currency.upper()}")
+
+
+@click.command()
 @click.option("--coin_id", default="bitcoin")
 @click.option("--currency", default="usd")
 def show_coin_price(coin_id, currency):
@@ -34,7 +75,12 @@ def show_coin_price(coin_id, currency):
 
 
 cli.add_command(show_coin_price)
+cli.add_command(add_investment)
+cli.add_command(get_investment_value)
 
 
 if __name__ == "__main__":
+    database = sqlite3.connect("portfolio.db")
+    cursor = database.cursor()
+    cursor.execute(CREATE_INVESTMENTS_SQL)
     cli()
