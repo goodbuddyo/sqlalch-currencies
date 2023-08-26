6495581a3dca4400402cc8913595bd2e1c1e4b2f
Seano <sean01234@yahoo.com>
2023-08-25 22:37:53 -0400

    add click group

--- main.py
+++ main.py
@@ -2,15 +2,39 @@ import requests
 import click
 
 
-@click.command()
-@click.option("--coin_id", default="bitcoin")
-@click.option("--currency", default="usd")
+CREATE_INVESTMENTS_SQL = """
+CREATE TABLE IF NOT EXISTS investments (
+    coin_id TEXT,
+    currency TEXT,
+    amount REAL,
+    sell INT, 
+    date TIMESTAMP
+);
+"""
+
+
 def get_coin_price(coin_id, currency):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={currency}"
     data = requests.get(url).json()
     coin_price = data[coin_id][currency]
+    return coin_price
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option("--coin_id", default="bitcoin")
+@click.option("--currency", default="usd")
+def show_coin_price(coin_id, currency):
+    coin_price = get_coin_price(coin_id, currency)
     print(f"The price of {coin_id} is {coin_price:.2f} {currency.upper()}")
 
 
+cli.add_command(show_coin_price)
+
+
 if __name__ == "__main__":
-    get_coin_price()
+    cli()
